00069293132860ee5c234bb204204c9097e76843
melanieaqui <melanie.wayuaquino@gmail.com>
2024-06-14 08:25:52 +0800

    session fixes, comment out upload sa register

--- web_app/server.py
+++ web_app/server.py
@@ -97,47 +97,54 @@ def upload_profile_pic():
 # Route to handle login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    msg = ''
-    if request.method == 'POST' and 'pass' in request.form and 'user' in request.form:
-        if not limit_attempts():
-            msg = 'Too many login attempts. Please try again later.'
-            return render_template('login.html', msg=msg)
+    #if session and session['loggedIn']:
 
-        password = request.form['pass']
-        user = request.form['user']
+        msg = ''
+        if request.method == 'POST' and 'pass' in request.form and 'user' in request.form:
+            if not limit_attempts():
+                msg = 'Too many login attempts. Please try again later.'
+                return render_template('login.html', msg=msg)
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE email = %s', (user,))
-        account = cursor.fetchone()
+            password = request.form['pass']
+            user = request.form['user']
 
-        if account and bcrypt.checkpw(password.encode('utf-8'), account['password'].encode('utf-8')):
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['email'] = account['email']
-            msg = 'Logged in successfully!'
-            return redirect('/')
-        else:
-            msg = 'Incorrect username / password!'
-            # Increment failed attempts for the current IP
-            client_ip = request.remote_addr
-            if client_ip in login_attempts:
-                login_attempts[client_ip][0] += 1
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE email = %s', (user,))
+            account = cursor.fetchone()
+
+            if account and bcrypt.checkpw(password.encode('utf-8'), account['password']):
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['email'] = account['email']
+                msg = 'Logged in successfully!'
+                return redirect('/')
             else:
-                login_attempts[client_ip] = [1, time.time()]
+                msg = 'Incorrect username / password!'
+                # Increment failed attempts for the current IP
+                client_ip = request.remote_addr
+                if client_ip in login_attempts:
+                    login_attempts[client_ip][0] += 1
+                else:
+                    login_attempts[client_ip] = [1, time.time()]
 
-    return render_template('login.html', msg=msg)
+        return render_template('login.html', msg=msg)
+    
 
 # Additional routes and functions as per your existing application...
 
 @app.route('/admin')
 def admin():
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM `cssecdv-mp`.accounts WHERE id =%s', (session['id'], ))
-    account = cursor.fetchone()
-    if account['admin'] == 1:  
-        return render_template('admin.html')
-    else: 
-        return redirect('/')
+    if session:
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM `cssecdv-mp`.accounts WHERE id =%s', (session['id'], ))
+        account = cursor.fetchone()
+        if account['admin'] == 1:  
+            return render_template('admin.html')
+        else: 
+            return redirect('/')
+    else:
+        return redirect('/login')
 
 @app.route('/logout')
 def logout():
@@ -159,42 +166,45 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    msg = ''
-    if request.method == 'POST' and 'psw-repeat' in request.form and 'psw' in request.form and 'email' in request.form and 'fname' in request.form and 'lname' in request.form and 'phone' in request.form:
-        fname = request.form['fname']
-        lname = request.form['lname']
-        password = request.form['psw']
-        reppass = request.form['psw-repeat']
-        email = request.form['email']
-        phone = request.form['phone']
-        salt = bcrypt.gensalt(rounds=20)
-        hashed = bcrypt.hashpw(bytes(password, 'utf-8'), salt)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z]+', fname):
-            msg = 'Invalid Name!'
-        elif not re.match(r'[A-Za-z]+', lname):
-            msg = 'Invalid Name!'
-        elif not re.match(r'^09\d{9}$', phone) and not re.match(r'^[+]{1}(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$', phone):
-            msg = "Invalid phone number"
-        elif password != reppass:
-            msg = "Passwords not matching"
-        elif not email or not password or not phone or not reppass or not fname or not lname:
+    if session:
+       return redirect('/')
+    else:
+        msg = ''
+        if request.method == 'POST' and 'psw-repeat' in request.form and 'psw' in request.form and 'email' in request.form and 'fname' in request.form and 'lname' in request.form and 'phone' in request.form:
+            fname = request.form['fname']
+            lname = request.form['lname']
+            password = request.form['psw']
+            reppass = request.form['psw-repeat']
+            email = request.form['email']
+            phone = request.form['phone']
+            salt = bcrypt.gensalt(rounds=20)
+            hashed = bcrypt.hashpw(bytes(password, 'utf-8'), salt)
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
+            account = cursor.fetchone()
+            if account:
+                msg = 'Account already exists!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z]+', fname):
+                msg = 'Invalid Name!'
+            elif not re.match(r'[A-Za-z]+', lname):
+                msg = 'Invalid Name!'
+            elif not re.match(r'^09\d{9}$', phone) and not re.match(r'^[+]{1}(?:[0-9\-\(\)\/\.]\s?){6,15}[0-9]{1}$', phone):
+                msg = "Invalid phone number"
+            elif password != reppass:
+                msg = "Passwords not matching"
+            elif not email or not password or not phone or not reppass or not fname or not lname:
+                msg = 'Please fill out the form!'
+            else:
+                cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s, False,NULL)', (fname, lname, email, phone, hashed))
+                mysql.connection.commit()
+                msg = 'You have successfully registered!'
+
+        elif request.method == 'POST':
             msg = 'Please fill out the form!'
-        else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s, False)', (fname, lname, email, phone, hashed))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
 
-    elif request.method == 'POST':
-        msg = 'Please fill out the form!'
-
-    return render_template('reg.html', msg=msg)
+        return render_template('reg.html', msg=msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
