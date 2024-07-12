0569249bc6f5c50d08bbdc0d4b97faf8e28b6989
melsaqui <melanie.wayuaquino@gmail.com>
2024-07-12 17:27:21 +0800

    organized a bit??

--- web_app/server.py
+++ web_app/server.py
@@ -161,32 +161,32 @@ def change_role(user_id):
             if account['admin'] == 1:  
                 cursor.execute('SELECT * FROM `cssecdv-mp`.accounts WHERE id =%s', (user_id, ))
                 target =cursor.fetchone()
-                if target and target['admin']==0:
+                if target:
                     newRole = not target['admin']
-                    cursor.execute(f'UPDATE `cssecdv-mp`.accounts SET `admin` ={newRole} WHERE id={user_id}' )
-                    mysql.connection.commit()
-                    return redirect('/admin')
-                elif target and target['admin']==1:
-                    #check how many admins 
-                    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-
-                    cursor.execute('SELECT * FROM `cssecdv-mp`.accounts WHERE `admin`=TRUE')
-                    adminCount= cursor.fetchall()
-                    newRole = not target['admin']
-                    if len(adminCount)>1:
+                    if target['admin']==0:
                         cursor.execute(f'UPDATE `cssecdv-mp`.accounts SET `admin` ={newRole} WHERE id={user_id}' )
                         mysql.connection.commit()
                         return redirect('/admin')
-                    else:
+                    elif target['admin']==1:
+                    #check how many admins 
+                        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+                        cursor.execute('SELECT * FROM `cssecdv-mp`.accounts WHERE `admin`=TRUE')
+                        adminCount= cursor.fetchall()
+                        newRole = not target['admin']
+                        if len(adminCount)>1:
+                            cursor.execute(f'UPDATE `cssecdv-mp`.accounts SET `admin` ={newRole} WHERE id={user_id}' )
+                            mysql.connection.commit()
+                            return redirect('/admin')
+                        else:
                         #won't flash so user might be clueless why it didn't change
-                        flash("We need to have at least one admin!!")
-                        return redirect('/admin')
-                        
+                            flash("We need to have at least one admin!!")
+                            return redirect('/admin')
+                    else:
+                        return redirect('/admin') 
                 else:
                     return redirect('/admin')
-
             else: 
-                return redirect('/admin')  
+                return redirect('/')  
         else: 
             return redirect('/admin')     
     else:
