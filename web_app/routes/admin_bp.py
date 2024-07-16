b21ba1757e74b859d1b7e36bf6f0f6120dd6d2b3
melsaqui <melanie.wayuaquino@gmail.com>
2024-07-16 14:09:50 +0800

    admin can change profile details of other users, added bootstrap to other pages for consistency

--- web_app/routes/admin_bp.py
+++ web_app/routes/admin_bp.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from controllers.AdminControllers import admin, change_role
+from controllers.AdminControllers import admin, change_role,edit
 
 admin_bp = Blueprint('admin_bp', __name__)
 admin_bp.route('/')(admin)
-admin_bp.route('/change-role/<int:user_id>', methods=['GET', 'POST'])(change_role)
-admin_bp.route('/<int:user_id>&<str:user_email>/edit')
+admin_bp.route('/change-role/<int:user_id>&<user_email>', methods=['GET', 'POST'])(change_role)
+admin_bp.route('/<int:target_id>&<target_email>/edit', methods = ['POST'])(edit)
